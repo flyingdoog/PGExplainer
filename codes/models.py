03e8ef6be389d20ed8aed88063181fa5dd1937d2
Dongsheng Luo <dul262@psu.edu>
2021-01-21 10:50:31 -0500

    Update models.py
    Remove duplicated definition of GCN layers

--- codes/models.py
+++ codes/models.py
@@ -7,49 +7,6 @@ class GCN(keras.Model):
     def __init__(self, input_dim, output_dim,**kwargs):
         super(GCN, self).__init__(**kwargs)
         usebias = args.bias
-        try:
-            hiddens = [int(s) for s in args.hiddens.split('-')]
-        except:
-            hiddens =[args.hidden1]
-
-        self.layers_ = []
-
-        layer0 = GraphConvolution(input_dim=input_dim,
-                                  output_dim=hiddens[0],
-                                  activation=tf.nn.relu,
-                                  bias=usebias)
-        self.layers_.append(layer0)
-        nhiddens = len(hiddens)
-        for _ in range(1,nhiddens):
-            layertemp = GraphConvolution(input_dim=hiddens[_-1],
-                                      output_dim=hiddens[_],
-                                      activation=tf.nn.relu,
-                                  bias=usebias)
-            self.layers_.append(layertemp)
-
-
-        self.pred_layer = tf.keras.layers.Dense(output_dim)
-        # self.layers_.append(pred_layer)
-        self.hiddens = hiddens
-
-    def call(self,inputs,training=None):
-        x, support  = inputs
-        for layer in self.layers_:
-            x = layer.call((x,support),training)
-        x = self.pred_layer(x)
-        return x
-
-
-
-from config import *
-from layers import *
-from metrics import *
-from tensorflow import keras
-
-class GCN(keras.Model):
-    def __init__(self, input_dim, output_dim,**kwargs):
-        super(GCN, self).__init__(**kwargs)
-        usebias = args.bias
         self.bn = args.bn
 
         try:
